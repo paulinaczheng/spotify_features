9b84b4d87be3e78a196b0bf9ef4a61b5817e0b28
Paulina Zheng <paulinaczheng@gmail.com>
2018-09-13 15:56:02 -0400

    add dash screenshots

--- spotify_package/__init__.py
+++ spotify_package/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from sqlalchemy import create_engine
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import sessionmaker
 import dash
 
 server = Flask(__name__)
@@ -12,6 +14,14 @@ server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(server)
 
 app = dash.Dash(__name__, server=server, url_base_pathname = '/')
+
+some_engine = create_engine('postgresql://scott:tiger@localhost/')
+
+# create a configured "Session" class
+Session = sessionmaker(bind=some_engine)
+
+# create a Session
+session = Session()
 
 from spotify_package import routes
 # from spotify_package.seed import *
